0552b11f4da66da752c9616c41b3baecbe547770
Foxie EdianiaK <140124460+FTEdianiaK@users.noreply.github.com>
2023-08-15 22:34:35 +0200

    :page_facing_up: Added license file and headers

--- startupComplete-desktop.py
+++ startupComplete-desktop.py
@@ -1,6 +1,29 @@
+# Startup Complete: Simple command line tool to let you know
+#                   when your PC's ready to go.
+# Copyright (C) 2023  Foxie EdianiaK a.k.a. F_TEK
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import subprocess
 import platform
 from alive_progress import alive_bar
+
+print("""Startup Complete  Copyright (C) 2023  Foxie EdianiaK a.k.a. F_TEK
+This program comes with ABSOLUTELY NO WARRANTY. This is free software,
+and you are welcome to redistribute it under certain conditions.
+For more details refer to the LICENSE file in the GitHub repository."""
+      + "\n" * 4)
 
 
 LOADED = True
